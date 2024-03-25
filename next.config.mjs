b8708406993e6588027e3df20e67b2d984393510
saadazghour <saad.azghour@gmail.com>
2024-03-25 17:03:07 +0000

    fix: nextConfig file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // distDir: "build",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
