b5019a24a8e2879c4d7b5c1da5d1ecd34d1a985d
saadazghour <saad.azghour@gmail.com>
2024-03-27 05:32:21 +0000

    CleanUp

--- src/services/db.ts
+++ src/services/db.ts
@@ -1,8 +1,5 @@
 const { Level } = require("level");
 
-// Create a database
-// const db = new Level<string, any>("./level-db", { valueEncoding: "json" });
-
 let db;
 
 try {
@@ -15,29 +12,5 @@ try {
 } catch (error) {
   console.error("Failed to open database", error);
 }
-
-// All asynchronous methods also support callbacks.
-
-// Add an entry with key 'a' and value 1
-// db.put("a", { x: 123 }, function (err: any) {
-//   if (err) throw err;
-
-//   db.get("a", function (err: any, value: any) {
-//     console.log(value); // { x: 123 }
-//   });
-// });
-
-// Delete the entry with key 'a'
-// db.del("a", { x: 123 }, function (err: any) {
-//   if (err) throw err;
-
-//   db.get("a", function (err: any, value: any) {
-//     console.log(value); // { x: 123 }
-//   });
-// });
-
-// const value = await db.get<string, string>("a", { valueEncoding: "utf8" });
-
-// console.log(value);
 
 module.exports = db;
