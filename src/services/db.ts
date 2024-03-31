2845b859b71a71bcffefb04646044614ca2308bc
saadazghour <saad.azghour@gmail.com>
2024-03-31 16:10:53 +0000

    refactor: DB initialization to use batch && improve handling

--- src/services/db.ts
+++ src/services/db.ts
@@ -11,52 +11,52 @@ const users: Record<string, User> = {
   muser3: { password: "mpassword3", blocked: true },
 };
 
+// Define the database path
+const dbPath = "./level-db";
+
 // Initialize the database instance to null
-// let dbInstance: Level<string, User> | null = null;
-export let dbInstance: any = null;
+export const dbInstance = new Level(dbPath, { valueEncoding: "json" });
 
 async function initializeDB() {
-  if (!dbInstance) {
-    try {
-      // Define the database path
-      const dbPath = "./level-db";
+  try {
+    console.log("Initializing database...");
 
-      // Initialize the database with JSON encoding for values
-      dbInstance = new Level(dbPath, { valueEncoding: "json" });
-      console.log("Initializing database...");
+    // When we call Object.entries(users), it converts the users object into an array where each element is itself an array of two elements.
 
-      // When we call Object.entries(users), it converts the users object into an array where each element is itself an array of two elements.
+    // Using 'batch' to insert multiple users at once
+    const ops = Object.entries(users).map(([username, userInfo]) => ({
+      type: "put",
+      key: username,
+      value: userInfo,
+    }));
 
-      for (const [username, userInfo] of Object.entries(users)) {
-        // So, for the first iteration, the call would effectively be:
-        // await db.put("muser1", { password: "mpassword1", blocked: false });
+    await dbInstance.batch(ops);
 
-        await dbInstance?.put(username, userInfo);
-        console.log(`Inserted user: ${username}`, userInfo);
-      }
-
-      console.log("Database initialized successfully.");
-    } catch (error) {
-      console.error("Failed to initialize database:", error);
-      if (error) {
-        dbInstance = new Level("./level-db", { valueEncoding: "json" });
-        console.log("Reopening database...");
-        await dbInstance.open();
-      }
-    }
+    console.log("Database initialized successfully.");
+  } catch (error) {
+    console.error("Failed to initialize database:", error);
   }
-
-  return dbInstance;
 }
 
-export const dbPromise = initializeDB();
+// Call to initialize the database
+initializeDB();
+
+export async function openDB() {
+  if (dbInstance.status === "closed") {
+    await dbInstance.open();
+  }
+}
+
+export async function closeDB() {
+  if (dbInstance.status === "open") {
+    await dbInstance.close();
+  }
+}
 
 // After initializing the database and inserting the users...
 export async function checkUser(username: string) {
-  const db = await dbPromise;
-
   try {
-    const user = await db?.get(username);
+    const user = await dbInstance.get(username);
     console.log(`User ${username} exists with data:`, user);
   } catch (error) {
     console.error(`User ${username} not found:`, error);
